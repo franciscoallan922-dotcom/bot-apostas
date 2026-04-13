f9d5d1834e91d566ce45c486a0403210b579457e
franciscoallan922-dotcom <franciscoallan922@gmail.com>
2026-04-13 19:32:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import telebot
 
 TOKEN = "8642961399:AAEtPcccUwt93IBVYEwBZAh0YcmoV6AxoZY"
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "🚀 Bot online!")
 
 @bot.message_handler(func=lambda message: True)
 def responder(message):
-    bot.reply_to(message, f"Seu ID: {message.chat.id}")
+    bot.send_message(message.chat.id, f"Seu ID: {message.chat.id}")
 
 print("Bot rodando...")
 
-bot.infinity_polling()
+# 🔥 ESSA LINHA É A CHAVE
+bot.infinity_polling(none_stop=True, interval=0)
